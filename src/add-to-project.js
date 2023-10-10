707cb9b7dba2d2bde387c6cf81e71ada7eb76a7b
Claas Augner <caugner@mozilla.com>
2023-10-10 15:29:32 +0200

    feat(add-to-project): support multiple queries

--- src/add-to-project.js
+++ src/add-to-project.js
@@ -9,32 +9,34 @@ async function main() {
   const projectOwner = process.env["ISSUE_PROJECT_OWNER"];
   const projectNumber = Number(process.env["ISSUE_PROJECT_NUMBER"]);
   const projectDelay = Number(process.env["ISSUE_PROJECT_DELAY"]) || 0;
+  const queries = process.env.ISSUE_PROJECT_QUERY.split("\n");
 
   const createdBefore = getWorkingDaysAgo(projectDelay);
 
-  const q = `${
-    process.env.ISSUE_PROJECT_QUERY
-  } -project:${projectOwner}/${projectNumber} created:<${createdBefore.toISOString()}`;
+  for (const query of queries) {
+    const q = `${query} -project:${projectOwner}/${projectNumber} created:<${createdBefore.toISOString()}`;
 
-  console.log(`Searching issues:\n${q}`);
-  const issues = await octokit.paginate(
-    octokit.rest.search.issuesAndPullRequests,
-    {
-      q,
-    },
-  );
-  console.log(`> Found ${issues.length} issues.`);
-
-  if (issues.length) {
-    console.log();
-    const projectId = await getProjectId(projectOwner, projectNumber);
-    console.log(
-      `Adding issues to project https://github.com/orgs/${projectOwner}/projects/${projectNumber} (${projectId}):`,
+    console.log(`Searching issues:\n${q}`);
+    const issues = await octokit.paginate(
+      octokit.rest.search.issuesAndPullRequests,
+      {
+        q,
+      },
     );
-    for (const issue of issues) {
-      console.log(`> - ${issue.html_url}`);
-      await addIssueToProject(projectId, issue.node_id);
+    console.log(`> Found ${issues.length} issues.`);
+
+    if (issues.length) {
+      console.log();
+      const projectId = await getProjectId(projectOwner, projectNumber);
+      console.log(
+        `Adding issues to project https://github.com/orgs/${projectOwner}/projects/${projectNumber} (${projectId}):`,
+      );
+      for (const issue of issues) {
+        console.log(`> - ${issue.html_url}`);
+        await addIssueToProject(projectId, issue.node_id);
+      }
     }
+    console.log();
   }
 }
 
